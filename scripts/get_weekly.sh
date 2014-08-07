fe8b3740db0540de3ac441bd4a6d7b4c4bb9d743
lyuehh <lyuehh@gmail.com>
2014-08-07 16:17:51 +0800

    rename dir, update get_weekly.sh

--- scripts/get_weekly.sh
+++ scripts/get_weekly.sh
@@ -3,7 +3,21 @@
 for i in `jq '.[].url' weekly.json | sed 's/"//g'`
 # for i in "http://bitcoinweekly.com/"
 do
-    name=`echo $i `
+    name=`echo $i | sed 's/http:\/\///g' | awk -F '.' '{print $1}'`
     id=`curl -s $i | hquery -p -r '$(".sample a").attr("href").split("/")[2]'`
-    echo $id
+    file="../weekly/${name}/${id}.csv"
+    echo "checking $name..."
+    if [ ! -e $file ];
+    then
+        # not exists, then get it
+        url="$i/issues/${id}"
+        # echo 'not exists'
+        # echo $i
+        # echo $id
+        # echo $name
+        # echo $file
+        echo "get $name issue $id ..."
+        curl -s $url | hquery -p -f 'get_weekly.js' | gsed '1 i title,link' > ../weekly/${name}/${id}.csv
+    fi
+
 done
